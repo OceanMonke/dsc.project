139cd08f7b966a5eee30ab6014f5773a97c4c100
LuciusCalvius <lukaszz.mitoraj@gmail.com>
2021-12-06 21:21:13 +0100

    Adding base project structure

---
+++ Dsc.Project.Domain/Services/IEmailFooterService.cs
@@ -0,0 +1,18 @@
+﻿using Dsc.Project.Domain.Data.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsc.Project.Domain.Services
+{
+    public interface IEmailFooterService
+    {
+
+
+        EmailFooterDto GetFooter()
+
+
+    }
+}
---
+++ Dsc.Project.Infrastructure/Services/EmailFooterService.cs
@@ -0,0 +1,18 @@
+﻿using Dsc.Project.Domain.Data.Dto;
+using Dsc.Project.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsc.Project.Infrastructure.Services
+{
+    public class EmailFooterService : IEmailFooterService
+    {
+        public EmailFooterDto GetFooter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
